1ae6ee64f49f867df4fbc4eb73ad4f2c2b10c1c4
Aliaksandr_Fedasiuk <Aliaksandr_Fedasiuk@epam.com>
2022-02-11 13:46:15 +0300

    MODSOURMAN-694: Improve sql query for retrieving job execution sourcechunks (#572)
    * MODSOURMAN-694: Improve sql query for retrieving job execution source chunks
    
    (cherry picked from commit 812f9ba3f58dc4be1ba117eb6845af86447c3efa)

--- mod-source-record-manager-server/src/main/java/org/folio/dao/JobExecutionSourceChunkDaoImpl.java
+++ mod-source-record-manager-server/src/main/java/org/folio/dao/JobExecutionSourceChunkDaoImpl.java
@@ -39,7 +39,7 @@ public class JobExecutionSourceChunkDaoImpl implements JobExecutionSourceChunkDa
 
   public static final Logger LOGGER = LogManager.getLogger();
   private static final String TABLE_NAME = "job_execution_source_chunks";
-  private static final String ID_FIELD = "'id'";
+  private static final String ID_FIELD = "id";
   private static final String JOB_EXECUTION_ID_FIELD = "'jobExecutionId'";
   private static final String IS_PROCESSING_COMPLETED_QUERY = "SELECT is_processing_completed('%s');";
   private static final String ARE_THERE_ANY_ERRORS_DURING_PROCESSING_QUERY = "SELECT processing_contains_error_chunks('%s');";
@@ -82,10 +82,9 @@ public class JobExecutionSourceChunkDaoImpl implements JobExecutionSourceChunkDa
         LOGGER.warn("Can't retrieve JobExecutionSourceChunk by empty id.");
         return promise.future().map(Optional.empty());
       }
-      String[] fieldList = {"*"};
-      CQLWrapper cql = getCQLWrapper(TABLE_NAME, "id = " + id, 0, 1);
+      Criteria idCrit = constructCriteria(ID_FIELD, id).setJSONB(false);
       pgClientFactory.createInstance(tenantId)
-        .get(TABLE_NAME, JobExecutionSourceChunk.class, fieldList, cql, true, false, promise);
+        .get(TABLE_NAME, JobExecutionSourceChunk.class, new Criterion(idCrit), true, false, promise);
     } catch (Exception e) {
       LOGGER.error("Error querying JobExecutionSourceChunk by id {}", id, e);
       promise.fail(e);
@@ -99,7 +98,7 @@ public class JobExecutionSourceChunkDaoImpl implements JobExecutionSourceChunkDa
   public Future<JobExecutionSourceChunk> update(JobExecutionSourceChunk jobExecutionChunk, String tenantId) {
     Promise<JobExecutionSourceChunk> promise = Promise.promise();
     try {
-      Criteria idCrit = constructCriteria(ID_FIELD, jobExecutionChunk.getId());
+      Criteria idCrit = constructCriteria(ID_FIELD, jobExecutionChunk.getId()).setJSONB(false);
       pgClientFactory.createInstance(tenantId)
         .update(TABLE_NAME, jobExecutionChunk, new Criterion(idCrit), true, updateResult -> {
           if (updateResult.failed()) {
